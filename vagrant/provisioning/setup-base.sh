0e8fc541050a463f6e17e057fe44391aa8dae6dc
Kyle Mestery <mestery@mestery.com>
2016-01-13 09:12:56 -0600

    Vagrant: Add an 8GB swapfile to all nodes
    This helps for memory sensitive items.
    
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- vagrant/provisioning/setup-base.sh
+++ vagrant/provisioning/setup-base.sh
@@ -15,3 +15,11 @@ fi
 if [ ! -d "networking-ovn" ]; then
     git clone http://git.openstack.org/openstack/networking-ovn.git
 fi
+
+# Temporary for swap space config
+sudo rm -f /swapfile1
+sudo dd if=/dev/zero of=/swapfile1 bs=1024 count=8388608
+sudo chown root:root /swapfile1
+sudo chmod 0600 /swapfile1
+sudo mkswap /swapfile1
+sudo swapon /swapfile1
